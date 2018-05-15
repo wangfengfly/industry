1feee46c71d5d88a1fbe25ace10769b48545d084
WangFeng(桌面事业部_商业平台部) <wangfeng211731@sogou-inc.com>
2018-05-15 10:45:25 +0800

    sql

--- output/industry/user_guide/项目文档/xa_2pc.sql
+++ output/industry/user_guide/项目文档/xa_2pc.sql
@@ -26,26 +26,27 @@ CREATE TABLE `city` (
 )ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COMMENT = '市';
 
 CREATE TABLE `projects` (
-  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(200) NOT NULL DEFAULT '',
-  `sshy1` INT(11) UNSIGNED NOT NULL DEFAULT 0,
-  `sshy2` INT(11) UNSIGNED NOT NULL DEFAULT 0,
-  `jsdw` VARCHAR(200) NOT NULL DEFAULT '' COMMENT '建设单位',
-  `jsdd` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '建设地点',
-  `tzztxz` VARCHAR(45) NOT NULL DEFAULT '' COMMENT '投资主体性质',
-  `tze` FLOAT NOT NULL DEFAULT 0 COMMENT '投资额',
-  `jsnr` TEXT NOT NULL DEFAULT '' COMMENT '建设内容',
-  `jjzb` TEXT NOT NULL DEFAULT '' COMMENT '经济指标',
-  `jssj1` INT(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '建设时间——起',
-  `jssj2` INT(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '建设时间—止',
-  `tags` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '标签',
-  `xmxz` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '0 未知 1新建 2改扩建 3融资',
-  `ssyq` INT(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '所属园区,关联园区表主键',
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL DEFAULT '',
+  `sshy1` int(11) unsigned NOT NULL DEFAULT '0',
+  `sshy2` int(11) unsigned NOT NULL DEFAULT '0',
+  `jsdw` varchar(200) NOT NULL DEFAULT '' COMMENT '建设单位',
+  `jsdd1` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '省',
+  `jsdd2` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '市',
+  `tzztxz` varchar(45) NOT NULL DEFAULT '' COMMENT '投资主体性质',
+  `tze` float NOT NULL DEFAULT '0' COMMENT '投资额',
+  `jsnr` text NOT NULL COMMENT '建设内容',
+  `jjzb` text NOT NULL COMMENT '经济指标',
+  `jssj1` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '建设时间——起',
+  `jssj2` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '建设时间—止',
+  `tags` varchar(100) NOT NULL DEFAULT '' COMMENT '标签',
+  `xmxz` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0 未知 1新建 2改扩建 3融资',
+  `ssyq` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '所属园区,关联园区表主键',
   PRIMARY KEY (`id`),
-  index `idx_sshy1_sshy2`(`sshy1`, `sshy2`),
-  index `idx_sshy2`(`sshy2`),
-  index `idx_jsdd`(`jsdd`)
-)ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COMMENT = '项目';
+  KEY `idx_sshy1_sshy2` (`sshy1`,`sshy2`),
+  KEY `idx_sshy2` (`sshy2`),
+  KEY `idx_jsdd` (`jsdd1`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='项目'
 
 CREATE TABLE `park` (
   `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
